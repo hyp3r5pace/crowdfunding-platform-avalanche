1630210e4fe7fcd99ed234b500ca3de6fb9fd8c7
Soumyajit Deb <debsoumyajit100@gmail.com>
2022-01-08 20:41:16 +0530

    Resolved security issue due to target=_blank in footer link

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -7,21 +7,21 @@ export default function FooterComponent(props) {
               <div className="footerContainer">
                 <IconContext.Provider value={{ style: {fontSize: '30px', color: "rgb(0, 123, 255)"}}}>
                     <div className="twitter-icon">
-                        <a href="https://twitter.com/crypto_gaijin" target="_blank">
+                        <a href="https://twitter.com/crypto_gaijin" target="_blank" rel="noreferrer">
                             <FaTwitter />
                         </a>
                     </div>
                 </IconContext.Provider>
                 <IconContext.Provider value={{ style: {fontSize: '30px', color: "rgb(0, 0, 0)"}}}>
                     <div className="github-icon">
-                        <a href="https://github.com/hyp3r5pace" target="_blank">
+                        <a href="https://github.com/hyp3r5pace" target="_blank" rel="noreferrer">
                             <FaGithub />
                         </a>
                     </div>
                 </IconContext.Provider>
                 <IconContext.Provider value={{ style: {fontSize: '30px', color: "rgb(138, 58, 185)"}}}>
                     <div className="insta-icon">
-                        <a href="https://www.instagram.com/sayan.kar2000/" target="_blank">
+                        <a href="https://www.instagram.com/sayan.kar2000/" target="_blank" rel="noreferrer">
                             <FaInstagram />
                         </a>
                     </div>
